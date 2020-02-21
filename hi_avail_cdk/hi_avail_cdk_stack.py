fa453924a857ba3b03fa058787e742adcbaff40b
Vince Bloise <vbloise3@gmail.com>
2020-02-21 07:31:27 -0500

    added appsync example

--- hi_avail_cdk/hi_avail_cdk_stack.py
+++ hi_avail_cdk/hi_avail_cdk_stack.py
@@ -1,123 +1,189 @@
-from aws_cdk import (
-    aws_ec2 as ec2,
-    aws_iam as iam,
-    aws_elasticloadbalancingv2 as elb,
-    aws_autoscaling as autoscaling,
-    core,
+from aws_cdk import core
+from aws_cdk.aws_appsync import (
+    CfnGraphQLSchema,
+    CfnGraphQLApi,
+    CfnApiKey,
+    CfnDataSource,
+    CfnResolver
 )
-
-# define EC2 type
-ec2_type = "t2.micro"
-linux_ami=ec2.AmazonLinuxImage()
-
-# read in userData, used to install httpd and start the httpd service on web EC2 instances
-with open("./user_data/user_data.sh") as f:
-    user_data = f.read()
+from aws_cdk.aws_dynamodb import (
+    Table,
+    Attribute,
+    AttributeType,
+    StreamViewType,
+    BillingMode
+)
+from aws_cdk.aws_iam import (
+    Role,
+    ServicePrincipal,
+    ManagedPolicy,
+    PolicyStatement
+)
 
 class HiAvailCdkStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, *, stack_tag="default", **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Configure the natGatewayProvider when defining a Vpc
-        vpc = ec2.Vpc(self, "TheVPC",
-            nat_gateways=3,
-            # Define our VPC
-            # 'cidr' configures the IP range and size of the entire VPC.
-            # The IP space will be divided over the configured subnets.
-            cidr="10.0.0.0/21",
+        # Define our table name
+        table_name = 'items2'
 
-            # 'maxAzs' configures the maximum number of availability zones to use
-            max_azs=3,
-
-            # 'subnetConfiguration' specifies the "subnet groups" to create.
-            # Every subnet group will have a subnet for each AZ, so this
-            # configuration will create 3 groups × 3 AZs = 9 subnets.
-            subnet_configuration=[ec2.SubnetConfiguration(
-                # 'subnetType' controls Internet access, as described above.
-                subnet_type=ec2.SubnetType.PUBLIC,
-
-                # 'name' is used to name this particular subnet group. You will have to
-                # use the name for subnet selection if you have more than one subnet
-                # group of the same type.
-                name="Pub",
-
-                # 'cidrMask' specifies the IP addresses in the range of of individual
-                # subnets in the group. Each of the subnets in this group will contain
-                # 2^(32 address bits - 24 subnet bits) - 2 reserved addresses = 254
-                # usable IP addresses.
-                #
-                # If 'cidrMask' is left out the available address space is evenly
-                # divided across the remaining subnet groups.
-                cidr_mask=24
-            ), ec2.SubnetConfiguration(
-                cidr_mask=24,
-                name="App",
-                subnet_type=ec2.SubnetType.PRIVATE
-            ), ec2.SubnetConfiguration(
-                cidr_mask=28,
-                name="DB",
-                subnet_type=ec2.SubnetType.ISOLATED,
-
-                # 'reserved' can be used to reserve IP address space. No resources will
-                # be created for this subnet, but the IP range will be kept available for
-                # future creation of this subnet, or even for future subdivision.
-                # reserved=True
-            )
-            ]
+        # Define our API
+        items_graphql_api = CfnGraphQLApi(
+            self, 'Items2Api',
+            name='items2-api',
+            authentication_type='API_KEY'
         )
 
-        # To use the traditional Bastion host without SSM
-        #host = ec2.BastionHostLinux(self, "BastionHost",
-        #    vpc=vpc,
-        #    subnet_selection=SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
-        #)
-        #host.allow_ssh_access_from(ec2.Peer.ipv4("1.2.3.4/32"))
+        CfnApiKey(
+            self, 'Items2ApiKey',
+            api_id=items_graphql_api.attr_api_id
+        )
 
-        # Use Bastion host with SSM
-        host = ec2.BastionHostLinux(self, "BastionHost", vpc=vpc)
+        # Define our API schema
+        api_schema = CfnGraphQLSchema(
+            self, 'Items2Schema',
+            api_id=items_graphql_api.attr_api_id,
+            definition=f"""\
+                type {table_name} {{
+                    {table_name}Id: ID!
+                    name: String
+                }}
+                type Paginated{table_name} {{
+                    items: [{table_name}!]!
+                    nextToken: String
+                }}
+                type Query {{
+                    all(limit: Int, nextToken: String): Paginated{table_name}!
+                    getOne({table_name}Id: ID!): {table_name}
+                }}
+                type Mutation {{
+                    save(name: String!): {table_name}
+                    delete({table_name}Id: ID!): {table_name}
+                }}
+                type Schema {{
+                    query: Query
+                    mutation: Mutation
+                }}"""
+        )
 
-        # Define the IAM role that will allow the EC2 instance to communicate with SSM
-        role = iam.Role(self, "Role", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
-        # arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore
+        items_table = Table(
+            self, 'Items2Table',
+            table_name=table_name,
+            partition_key=Attribute(
+                name=f'{table_name}Id',
+                type=AttributeType.STRING
+            ),
+            billing_mode=BillingMode.PAY_PER_REQUEST,
+            stream=StreamViewType.NEW_IMAGE,
+
+            # The default removal policy is RETAIN, which means that cdk
+            # destroy will not attempt to delete the new table, and it will
+            # remain in your account until manually deleted. By setting the
+            # policy to DESTROY, cdk destroy will delete the table (even if it
+            # has data in it)
+            removal_policy=core.RemovalPolicy.DESTROY # NOT recommended for production code
+        )
+
+        items_table_role = Role(
+            self, 'ItemsDynamoDBRole',
+            assumed_by=ServicePrincipal('appsync.amazonaws.com')
+        )
+
         role_id="mp" + stack_tag
-        policy_name="AmazonSSMManagedInstanceCore" + stack_tag
-        role.add_managed_policy(iam.ManagedPolicy(self, id=role_id, managed_policy_name=policy_name,
-                                                  statements=[iam.PolicyStatement(actions=['*'], resources=['*'])]))
+        policy_name="AmazonDynamoDBFullAccess" + stack_tag
 
-        # Create ALB in our VPC. Set internet_facing to 'true'
-        # to create an external application load balancer.
-        alb = elb.ApplicationLoadBalancer(self, "myALB",
-                                          vpc=vpc,
-                                          internet_facing=True,
-                                          load_balancer_name="myALB"
-                                          )
-        # allow internet access to port 80
-        alb.connections.allow_from_any_ipv4(
-            ec2.Port.tcp(80), "Internet access ALB via port 80")
-        # listen on port 80
-        listener = alb.add_listener("my80",
-                                    port=80,
-                                    open=True)
+        items_table_role.add_managed_policy(ManagedPolicy(self, id=role_id, managed_policy_name=policy_name,
+                                                  statements=[PolicyStatement(actions=['*'], resources=['*'])]))
 
-        asg = autoscaling.AutoScalingGroup(self, "myASG",
-                                                vpc=vpc,
-                                                vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE),
-                                                instance_type=ec2.InstanceType(instance_type_identifier=ec2_type),
-                                                machine_image=linux_ami,
-                                                role=role,
-                                                user_data=ec2.UserData.custom(user_data),
-                                                desired_capacity=3,
-                                                min_capacity=3,
-                                                max_capacity=6,
-                                                )
+        # Define our items data source
+        data_source = CfnDataSource(
+            self, 'Items2DataSource',
+            api_id=items_graphql_api.attr_api_id,
+            name='Items2DynamoDataSource',
+            type='AMAZON_DYNAMODB',
+            dynamo_db_config=CfnDataSource.DynamoDBConfigProperty(
+                table_name=items_table.table_name,
+                aws_region=self.region
+            ),
+            service_role_arn=items_table_role.role_arn
+        )
 
-        asg.connections.allow_from(alb, ec2.Port.tcp(80), "ALB access port 80 of EC2 in Autoscaling Group")
-        listener.add_targets("addTargetGroup",
-                             port=80,
-                             targets=[asg])
+        # Define our resolvers
+        get_one_resolver = CfnResolver(
+            self, 'GetOneQueryResolver',
+            api_id=items_graphql_api.attr_api_id,
+            type_name='Query',
+            field_name='getOne',
+            data_source_name=data_source.name,
+            request_mapping_template=f"""\
+            {{
+                "version": "2017-02-28",
+                "operation": "GetItem",
+                "key": {{
+                "{table_name}Id": $util.dynamodb.toDynamoDBJson($ctx.args.{table_name}Id)
+                }}
+            }}""",
+            response_mapping_template="$util.toJson($ctx.result)"
+        )
 
-        # Scale to keep the CPU usage of your instances at around 50% utilization
-        asg.scale_on_cpu_utilization("KeepSpareCPU",
-                                     target_utilization_percent=50
-                                     )
+        get_one_resolver.add_depends_on(api_schema)
+
+        get_all_resolver = CfnResolver(
+            self, 'GetAllQueryResolver',
+            api_id=items_graphql_api.attr_api_id,
+            type_name='Query',
+            field_name='all',
+            data_source_name=data_source.name,
+            request_mapping_template=f"""\
+            {{
+                "version": "2017-02-28",
+                "operation": "Scan",
+                "limit": $util.defaultIfNull($ctx.args.limit, 20),
+                "nextToken": $util.toJson($util.defaultIfNullOrEmpty($ctx.args.nextToken, null))
+            }}""",
+            response_mapping_template="$util.toJson($ctx.result)"
+        )
+
+        get_all_resolver.add_depends_on(api_schema)
+
+        save_resolver = CfnResolver(
+            self, 'SaveMutationResolver',
+            api_id=items_graphql_api.attr_api_id,
+            type_name='Mutation',
+            field_name='save',
+            data_source_name=data_source.name,
+            request_mapping_template=f"""\
+            {{
+                "version": "2017-02-28",
+                "operation": "PutItem",
+                "key": {{
+                    "{table_name}Id": {{ "S": "$util.autoId()" }}
+                }},
+                "attributeValues": {{
+                    "name": $util.dynamodb.toDynamoDBJson($ctx.args.name)
+                }}
+            }}""",
+            response_mapping_template="$util.toJson($ctx.result)"
+        )
+
+        save_resolver.add_depends_on(api_schema)
+
+        delete_resolver = CfnResolver(
+            self, 'DeleteMutationResolver',
+            api_id=items_graphql_api.attr_api_id,
+            type_name='Mutation',
+            field_name='delete',
+            data_source_name=data_source.name,
+            request_mapping_template=f"""\
+            {{
+                "version": "2017-02-28",
+                "operation": "DeleteItem",
+                "key": {{
+                "{table_name}Id": $util.dynamodb.toDynamoDBJson($ctx.args.{table_name}Id)
+                }}
+            }}""",
+            response_mapping_template="$util.toJson($ctx.result)"
+        )
+
+        delete_resolver.add_depends_on(api_schema)
